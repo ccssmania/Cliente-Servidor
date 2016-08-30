5811995f54460cca8a740e20b75fc58f8bdcfeeb
ccssmania <ccss@utp.edu.co>
2016-08-30 12:12:16 -0500

    Add files via upload

---
+++ chat.cc
@@ -0,0 +1,81 @@
+#include <cassert>
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <zmqpp/zmqpp.hpp>
+
+using namespace std;
+using namespace zmqpp;
+//using json = nlohmann::json;
+
+vector<string> tokenize(string &input) {
+  stringstream ss(input);
+  vector<string> result;
+  string s;
+  while (ss >> s)
+    result.push_back(s);
+  return result;
+}
+
+int main(int argc, char const *argv[]) {
+  if (argc != 4) {
+    cerr << "Invalid arguments" << endl;
+    return EXIT_FAILURE;
+  }
+  string address(argv[1]);
+  string userName(argv[2]);
+  string password(argv[3]);
+  string sckt("tcp://");
+  sckt += address;
+
+  context ctx;
+  socket s(ctx, socket_type::xrequest);
+
+  cout << "Connecting to: " << sckt << endl;
+  s.connect(sckt);
+
+  //cout << "hola";
+
+  message login;
+  login << "login" << userName << password;
+  s.send(login);
+
+  int console = fileno(stdin);
+  poller poll;
+  poll.add(s, poller::poll_in);
+  poll.add(console, poller::poll_in);
+  while (true) {
+    if (poll.poll()) { // There are events in at least one of the sockets
+      if (poll.has_input(s)) {
+        // Handle input in socket
+        message m;
+        s.receive(m);
+        //cout <<"PARTES DEL MSG : " << m.parts() << endl;
+        string text;
+        string aux;
+        string name;
+        for(int i = 0; i < m.parts() - 1; i++){
+          m >> aux;
+          text += aux + " ";
+        }
+        m >> name;
+        //cout << name << " asfasdfa " << m.parts() << endl;
+        cout << name <<" say : " << text << endl;
+      }
+      if (poll.has_input(console)) {
+        // Handle input from console
+        string input;
+        getline(cin, input);
+        vector<string> tokens = tokenize(input);
+        message m;
+        for (const string &str : tokens)
+          m << str;
+        m << userName;
+        s.send(m);
+      }
+    }
+  }
+  return EXIT_SUCCESS;
+}
---
+++ chat_server.cc
@@ -0,0 +1,337 @@
+//#include "json.hpp"
+#include <cassert>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <zmqpp/zmqpp.hpp>
+#include <sstream>
+#include <SFML/Audio.hpp>
+
+using namespace std;
+using namespace zmqpp;
+
+class User {
+private:
+  string name;
+  string password;
+  string netId;
+  bool connected;
+  list<string> contacts;
+
+public:
+  User() {}
+  User(const string &name, const string &pwd, const string &id)
+      // Attribute initialization
+      : name(name),
+        password(pwd),
+        netId(id),
+        connected(false) {}
+  bool isPassword(const string &pwd) const { return password == pwd; }
+  void connect(const string &id) {
+    connected = true;
+    netId = id;
+  }
+
+  const string &identity() const{
+    return netId;
+  }
+
+  const string &userName() const{
+    
+    return name;
+  }
+
+  bool state(){
+    return connected;
+  }
+};
+
+class ServerState {
+private:
+  // Connected users
+  unordered_map<string, User> users;
+  unordered_map<string, list<User>> groups;
+  socket &sckt;
+
+public:
+  //serverState() {}
+  ServerState(socket &s): sckt(s){}
+  
+  void send(message &m){
+    sckt.send(m);
+  }
+
+  void newUser(const string &name, const string &pwd, const string &id) {
+    // TODO: check that the user does not exist.
+    users[name] = User(name, pwd, id);
+    //users[id] = User(name, pwd, id);
+  }
+  bool login(const string &name, const string &pwd, const string &id) {
+    if (users.count(name) > 0) {
+      // User is registered
+      bool ok = users[name].isPassword(pwd);
+      if (ok)
+        users[name].connect(id);
+      return ok;
+    }
+    return false;
+  }
+
+  bool exist(const string &nombre){
+    if(users[nombre].userName() == nombre) return true;
+    else false;
+  }
+
+  bool conectado(string &name){
+    if(users[name].state() == true) return true;
+
+  }
+
+  void sendMessage(const string &dest, const string &text, const string &sender){
+    message m;
+    //users[dest].identity;
+    cout << "estoy en server sendMessage" << endl;
+    cout << "NOMBRE :" << sender << endl;
+    m << users[dest].identity() << text << sender ;
+
+    send(m);
+  }
+
+  void newGroup(const string& creator, const string& name) {
+        if (groups.count(name)) {
+            cerr << "Group " << name << " is already created" << endl;
+        } else {
+            groups[name].push_front(users[creator]);
+            cout << "Group created" << endl;
+        }
+    }
+
+    void addToGroup(string& nameGroup, string& name, const string &sender) {
+        message res;
+        if (!groups.count(name)) {
+            groups[nameGroup].push_front(users[name]);
+            //res << sender << "Se a unido al grupo exitosamente";
+        } else {
+            cout << "Group name does not exist" << endl;
+            //res << "Group name does not exist";
+        }
+        
+    }
+
+
+
+     bool isGroup(const string &groupName){
+        if(groups.count(groupName))return true;
+        else return false;
+     }
+
+     bool isGroup_name(string& name) {
+        string groupName;
+        for(unordered_map<string, list <User>>::iterator i = groups.begin(); i != groups.end(); i++){
+            for(auto& j : i->second){
+                if(j.userName() == name) return true;
+            }
+        }
+        return false;
+        
+    }
+
+    string nameGroup(const string& name) {
+        for (unordered_map<string, list<User>>::iterator it = groups.begin();
+             it != groups.end(); ++it) {
+            for (const auto& user : it->second) {
+                // if (user.userName() != it->first) {
+                if(user.userName() == name)
+                  return it->first;
+                
+            }
+        }
+    }
+
+    /*int number_of_groups(const string &name){
+      int count;
+      for(unordered_map<string, list <User>>::iterator i = groups.begin(); i != groups.end(); i++){
+            for(auto& j : i->second){
+                if(j.userName() == name) count ++;
+            }
+        }
+        return count;
+    }*/
+    void sendGroupMessage(const string& dest, const string& nombre,
+                          const string& text, const string &sender) {
+        cout << "tex grupos " << text << endl;
+        for (const auto& user : groups[dest]) {
+            if (user.identity() != sender) {
+                message m;
+                cout << user.userName() << " " << endl;
+                m << user.identity() << "in Group " << dest << " :" << text << nombre;
+                send(m);
+            }
+        }
+    }
+  
+};
+
+void sendMessage(message &msg, const string dest, const string &sender, ServerState &server){
+  cout << "msg parts :"<< msg.parts() << endl;
+
+  string text;
+  string aux;
+  for (int i = 0; i < msg.parts() -3; i++){
+    
+    msg >> aux;
+    text += aux + " ";
+  }
+  string name;
+  msg >> name;
+  server.sendMessage(dest, text, name);
+}
+
+void login(message &msg, const string &sender, ServerState &server) {
+
+  string userName;
+  msg >> userName;
+
+  string password;
+  msg >> password;
+  
+  if (server.login(userName, password, sender)) {
+    cout << "User " << userName << " joins the chat server" << endl;
+
+  } else {
+      cerr << "Wrong user/password " << endl;
+  }
+}
+
+void singUp(message &msg, const string &sender, ServerState &server){
+  string userName;
+  msg >> userName;
+  string password;
+  msg >> password;
+  server.newUser(userName, password, sender); 
+}
+
+/*void sendMessage_group(const string &name, const string text, ServerState &server){
+  server.sendMessage_group(name,text);
+}*/
+
+
+void dispatch(message &msg, ServerState &server) {
+  if(msg.parts() > 1){
+  
+      string sender;
+      msg >> sender;
+
+      string action;
+      msg >> action;
+
+      cout <<"action " << action << endl;
+
+      //cout << " sender : " << sender << endl;
+      bool estado = server.conectado(action);
+      cout << "estado :" << estado << endl;
+
+      if (action == "login") {
+
+        login(msg, sender, server);
+
+      } else if(server.conectado(action) == true && server.exist(action)== true){
+
+
+        sendMessage(msg, action, sender, server);
+
+      } else if(action == "singUp"){
+
+        singUp(msg, sender, server);
+
+      } else if(action == "newUser" ){
+
+        string name;
+        msg >> name;
+        string password;
+        msg >> password;
+
+        server.newUser(name,password,sender);
+
+      } else if(action == "newGroup"){
+
+        string name_group;
+        msg >> name_group;
+        string name_creater;
+        msg >> name_creater;
+
+        server.newGroup(name_creater, name_group);
+
+        //cout << "Grupo creado exitosamente" << endl;
+
+
+      } else if(action == "addToGroup"){
+          string nameGroup;
+          msg >> nameGroup;
+
+          string user;
+          msg >> user;
+          server.addToGroup(nameGroup, user, sender);
+          cout << "El usuario " << user << " A accedido al grupo : " << nameGroup << endl;
+
+       } else if(msg.parts() >=2){
+            string aux;
+            string text= action + " ";
+            for(int i = 0; i < msg.parts()-3; i++){
+                msg >> aux;
+                text += aux + " ";
+            }
+
+            string name;
+            cout << "text : " << text << endl;
+            msg >> name;
+            if(server.isGroup(action)){
+                
+                server.sendGroupMessage(action, name, text, sender);
+
+          }else if (server.isGroup_name(name)){
+              
+                
+                string name_group= server.nameGroup(name);
+              
+                cout << "NOMBRE : " << name << endl;
+                server.sendGroupMessage(name_group, name, text, sender);
+
+        } else {
+            cerr << "Action not supported/implemented" << endl;
+              message reply;
+              reply << sender << " unsupported " << action;
+              server.send(reply);
+        }
+    }
+  }else{
+      string sender;
+      msg >> sender;
+
+      message m;
+      m << sender << "operacion invalida";
+      server.send(m);
+
+    }     
+  
+}
+
+
+int main(int argc, char *argv[]) {
+  context ctx;
+  socket s(ctx, socket_type::xreply);
+  s.bind("tcp://*:4242");
+
+  ServerState state(s);
+  state.newUser("Gustavo", "123", "");
+  state.newUser("Choque", "123", "");
+
+
+  while (true) {
+    message req;
+    s.receive(req);
+
+    dispatch(req, state);
+  }
+  cout << "Finished." << endl;
+}

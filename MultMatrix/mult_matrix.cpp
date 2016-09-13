ad608d3b22350bb450c1815584ce58814b76e667
Cristian Camilo Salazar Salazar <ccss@utp.edu.co>
2016-09-12 21:36:39 -0500

    multiplicacion de matrices

--- MultMatrix/mult_matrix.cpp
+++ MultMatrix/mult_matrix.cpp
@@ -1,11 +1,12 @@
 #include <time.h>
 #include <iostream>
 #include <thread>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-void show_matrix(vector<vector<long long int> > &m1) {
+void show_matrix(vector<vector<int> > &m1) {
   for (int i = 0; i < m1[i].size(); i++) {
     for (int j = 0; j < m1.size(); j++) {
       cout << m1[i][j] << " ";
@@ -14,48 +15,68 @@ void show_matrix(vector<vector<long long int> > &m1) {
   }
 }
 
-void mult_matrix_profe(vector<vector<int> > m, vector<int> coll, int rows,
-                       vector<vector<long long int> > &res) {
-  int sum = 0;
-  vector<long long int> aux;
-  for (int i = 0; i < rows; i++) {
+void mult_matrix_profe(const vector<vector<int> > &m, const vector<int> &coll,
+                       int colls, vector<int> &res, bool &estado) {
+  vector<int> aux;
+  for (int i = 0; i < colls; i++) {
+    int sum = 0;
     for (int j = 0; j < coll.size(); j++) {
       sum += m[i][j] * coll[j];
       // cout << "sum : " << sum << endl;
     }
     // cout << "hola" << endl;
-    aux.push_back(sum);
+    res[i] = sum;
   }
-  res.push_back(aux);
+  estado = true;
 }
-
-int main() {
-  vector<vector<int> > m1;
-  vector<vector<int> > m2;
-  vector<vector<long long int> > res;
-  vector<thread *> hilo(10000);
-  int rows, colls;
-
-  for (int i = 1; i <= 1000; i++) {
-    vector<int> v;
-    for (int j = 1; j <= 1000; j++) {
-      v.push_back(i + i);
+void dijtra_matrix_profe(const vector<vector<int> > &m, const vector<int> &coll,
+                         int rows, vector<int> &res, bool *estado) {
+  vector<int> aux;
+  for (int i = 0; i < rows; i++) {
+    int mn = std::numeric_limits<int>::max();
+    for (int j = 0; j < coll.size(); j++) {
+      mn = min(mn, m[i][j] + coll[j]);
+      // cout << "sum : " << sum << endl;
     }
-    m1.push_back(v);
+    // cout << "hola" << endl;
+    res[i] = mn;
   }
-  for (int i = 1; i <= 1000; i++) {
-    vector<int> v;
-    for (int j = 1; j <= 1000; j++) {
-      v.push_back(i + i);
+  estado = true;
+}
+int main() {
+  int size = 1000;
+  vector<vector<int> > m1(size, vector<int>(size));
+  vector<vector<int> > m2(size, vector<int>(size));
+  vector<vector<int> > res(size, vector<int>(size));
+  vector<thread *> hilo(hardware_concurrency());
+  std::vector<bool *> estado(hardware_concurrency(), true);
+
+  for (int i = 1; i <= size; i++) {
+    for (int j = 1; j <= size; j++) {
+      m1[i][j] = i + i;
     }
-    m2.push_back(v);
+  }
+  for (int i = 1; i <= size; i++) {
+    for (int j = 1; j <= size; j++) {
+      m2[i][j] = i + i;
+    }
   }
   clock_t t;
   t = clock();
-  for (int i = 0; i < 1000; i++) {
-    hilo[i] = new thread(mult_matrix_profe, m1, m2[i], 1000, ref(res));
+  int i = 0;
+  int count = 0;
+  while (true) {
+    if (estado[i] == true) {
+      hilo[i] = new thread(mult_matrix_profe, cref(m1), cref(m2[count]), size,
+                           ref(res[count]), ref(*estado[i]));
+      estado[i] = false;
+    }
+    if (i == 3) i = 0;
+    if (count == size - 1) break;
+    i++;
+    count++;
   }
-  for (int i = 0; i < 1000; i++) {
+  for (int i = 0; i < hilo.size(); i++) {
     hilo[i]->join();
   }
   t = clock() - t;
